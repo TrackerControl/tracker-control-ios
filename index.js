fe09c6895193643b3aff8f2f290a1c057af5d933
Konrad Kollnig <konradko96@gmail.com>
2023-09-08 14:41:39 +0200

    re-arrange code

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ require('dotenv').config();
 // Load the actual app
 const app = require('./server');
 
+// tell Express that we're behind a proxy (in production) so that it resolves internal URLs correctly
+var env = process.env.NODE_ENV || 'development';
+if (env == 'production')
+  app.enable('trust proxy');
+
 // Server express HTTP server
 const port = process.env.PORT || 443;
 const server = app.listen(port, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
-
-// tell Express that we're behind a proxy (in production) so that it resolves internal URLs correctly
-var env = process.env.NODE_ENV || 'development';
-if (env == 'production')
-  app.enable('trust proxy');
