7538b3fda8324ec230eadbc6df088c444d8c84ce
Konrad Kollnig <konradko96@gmail.com>
2023-09-12 01:07:23 +0200

    add uploading of errors, server-side

--- analyser/processQueue.sh
+++ analyser/processQueue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# make sure to be in correct working directory
+# make sure to be in correct working directory (e.g. if running from systemctl)
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 
@@ -9,16 +9,23 @@ if [ ! -f .env ]; then
   export $(cat .env | xargs)
 fi
 
+FRIDA_PATH=/home/pi/.local/bin
 SERVER=http://localhost:3000
 TEST_TIME=30
 ANALYSIS_VERSION=2
-UPLOAD_PASSWORD=
 PASS=1
-TIMEOUT=180
+TIMEOUT=300
+UPLOAD_PASSWORD=
+APPLE_EMAIL=
+APPLE_PASS=
 
 mkdir -p ipas
 mkdir -p classes
 mkdir -p analysis
+
+# create empty log file
+log="./processing.log"
+> $log
 
 killwait ()
 {
@@ -31,23 +38,17 @@ cleanup()
 	rm classes/$1-classes.txt
 	rm analysis/$1.json
 	rm ipas/$1.ipa
-	./helpers/ios_uninstall_all.sh
 	rm ipas/*.tmp
+	./helpers/ios_uninstall_all.sh
 }
 
 download()
 {
-	timeout $TIMEOUT ipatool download -b $1 --purchase -o ./ipas/$1.ipa --non-interactive --keychain-passphrase $PASS
+	timeout $TIMEOUT ipatool download -b $1 --purchase -o ./ipas/$1.ipa --non-interactive --keychain-passphrase $PASS >> $log 2>&1
 }
 
-sleepsixty()
+sleepabit()
 {
-	echo "Sleeping for 60s"
-	sleep 10
-	echo "Sleeping for 50s"
-	sleep 10
-	echo "Sleeping for 40s"
-	sleep 10
 	echo "Sleeping for 30s"
 	sleep 10
 	echo "Sleeping for 20s"
@@ -72,36 +73,40 @@ while true; do
 
 		if [ ! -f "$f" ]; then
 			echo "Download failed. Trying again.."
-			sleepsixty
+			sleepabit
 			download $appId
 		fi
 
 		if [ ! -f "$f" ]; then
 			echo "Download failed. Trying again.."
-			sleepsixty
+			sleepabit
+			ipatool auth login --email $APPLE_EMAIL  --keychain-passphrase 1 --password $APPLE_PASS
+			sleep 10
 			download $appId
 		fi
 
 		if [ -f "$f" ]; then
-	   		ideviceinstaller -i $f
+	   		ideviceinstaller -i $f >> $log 2>&1
 
-			frida -U -f $appId -l ./helpers/find-all-classes.js > "classes/$appId-classes.txt" &
+			$FRIDA_PATH/frida -U -f $appId -l ./helpers/find-all-classes.js > "classes/$appId-classes.txt" 2>> $log &
 			PID2=$!
 			sleep $TEST_TIME
 			killwait $PID2
-			ideviceinstaller -U $appId
+			ideviceinstaller -U $appId >> $log 2>&1
 
-			./static_analysis.py $appId
+			./static_analysis.py $appId >> $log 2>&1
 			curl -s "$SERVER/uploadAnalysis?password=$UPLOAD_PASSWORD&appId=$appId&analysisVersion=$ANALYSIS_VERSION" -d @analysis/$appId.json -H "Content-Type: application/json"
 		fi
 
 		if [ ! -f "analysis/$appId.json" ]; then
-			curl -s "$SERVER/reportAnalysisFailure?password=$UPLOAD_PASSWORD&appId=$appId&analysisVersion=$ANALYSIS_VERSION"
+  			log_content=$(cat $log)
+			log_json=$(echo -e "{\"logs\": \"$log_content\"}" | sed 's/"/\\"/g')
+   			curl -s "$SERVER/reportAnalysisFailure?password=$UPLOAD_PASSWORD&appId=$appId&analysisVersion=$ANALYSIS_VERSION" -d "$log_json" -H "Content-Type: application/json"
 		fi
 
 		cleanup $appId
 	fi
 
-	sleepsixty
+	sleepabit
 done
 
